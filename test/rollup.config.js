3c2042c102a1e39e0061dca9b065a3cb63f2c186
Nikolas Savvidis <nicos.savidis@gmail.com>
2019-05-16 04:52:16 +0200

    rollup config test

--- test/rollup.config.js
+++ test/rollup.config.js
@@ -4,6 +4,7 @@ export default {
   input: 'test/app.js',
   external: [
     'lodash',
+    'bss',
     'mithril',
     'turbolinks'
   ],
@@ -16,13 +17,27 @@ export default {
       globals: {
         lodash: '_',
         mithril: 'm',
+        bss: 'b',
         turbolinks: 'Turbolinks'
       }
     }
   ],
   plugins: [
     depInject({
-      index: 'test/index.html'
+      index: 'test/index.html',
+      attr: 'defer', // add a 'defer' attribute to each generated script tag
+      ignore: [
+        'lodash' // do not inject the 'lodash' module
+      ],
+      unpkg: {
+        // leverage unpkg-uri and retrive the minified version
+        bss: '[min.js]'
+      },
+      custom: {
+        // use a different CDN for the 'mithril' module external
+        mithril:
+          'https://cdnjs.cloudflare.com/ajax/libs/mithril/1.1.6/mithril.min.js'
+      }
     })
   ],
   watch: {
