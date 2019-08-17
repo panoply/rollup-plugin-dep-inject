6f6e7fd630a08dee87261e4c3b458d4ecf82efff
Nikolas Savvidis <nicos.savidis@gmail.com>
2019-08-17 19:13:31 +0200

    additional updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,25 @@
 import buble from 'rollup-plugin-buble'
 
-var external = Object.keys(require('./package.json').dependencies).concat(
-  'path'
-)
+var external = Object.keys(require('./package.json').dependencies).concat('path')
 
 export default {
   input: 'src/index.js',
   plugins: [
     buble({
-      transforms: { templateString: false }
+      transforms: {
+        templateString: false
+      }
     })
   ],
   external: external,
   output: [
-    { file: 'dist/rollup-plugin-dep-inject.cjs.js', format: 'cjs' },
-    { file: 'dist/rollup-plugin-dep-inject.es6.js', format: 'esm' }
+    {
+      file: 'dist/rollup-plugin-dep-inject.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/rollup-plugin-dep-inject.es6.js',
+      format: 'esm'
+    }
   ]
 }
