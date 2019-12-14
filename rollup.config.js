cede3f94e44ee54fe7eab8e53321aed02230e18c
Nikolas Savvidis <nicos.savidis@gmail.com>
2019-12-14 06:16:56 +0100

    user terser and drop bublé

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,11 @@
-import buble from 'rollup-plugin-buble'
+import { terser } from 'rollup-plugin-terser'
 
 var external = Object.keys(require('./package.json').dependencies).concat('path')
 
 export default {
   input: 'src/index.js',
   plugins: [
-    buble({
-      transforms: {
-        templateString: false
-      }
-    })
+    terser()
   ],
   external: external,
   output: [
